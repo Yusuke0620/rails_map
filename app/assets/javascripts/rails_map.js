8f08903f3ecee4ed88c6d05f5f7dfc1c27b4a82a
Yusuke Akai <boston19851229@gmail.com>
2024-07-26 19:00:24 +0900

    :+1: Fix: JavaScriptのズーム値修正

--- app/assets/javascripts/rails_map.js
+++ app/assets/javascripts/rails_map.js
@@ -2,6 +2,6 @@ function initMap() {
 	const TanSonNhatAirportLocation = { lat: 10.8185, lng: 106.6518 };
   let map = new google.maps.Map(document.getElementById('map'), {
 	  center: TanSonNhatAirportLocation,
-	  zoom: 8  
+	  zoom: 15  
   });
 }
